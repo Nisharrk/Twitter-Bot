ba4bf7a2224ed8fe5a04f2a9302a9fe0b356ab5f
Your Name <you@example.com>
2022-08-23 12:32:04 -0400

    another finalll

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'L8fkXXz1CO8HYUeNQH7wJWC19',
-    consumer_secret: 'KTqm605E8LeylimDDANjelE4zxsadkfzADKxxaM51ZmmCh2EjX',
-    access_token: '1561822140332216320-g5BxK3SsGsCyChn2zhX2qaxc0hqNU1',
-    access_token_secret: 'Zti1Cz3BiAGi3Py8vHANJhkSi8B5NuBtUB7K1nwpcXaxA'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
 }
